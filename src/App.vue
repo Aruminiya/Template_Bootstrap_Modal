d11d39a8938b7ac0d8b832c0020176d26940e1e7
Leo <leo80103@gmail.com>
2024-01-28 17:00:17 +0800

    完成 Modal 樣板

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,25 @@
-<script></script>
+<script>
+import Modal from "./components/Modal.vue";
 
-<template>123</template>
+export default {
+  components: { Modal },
+  methods: {
+    modalClick() {
+      // Vue 可以透過 $refs 在父組件操作 子組件的方法或狀態
+      this.$refs.modalTemplate.modalShow();
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>按下按鈕打開 Modal</h1>
+    <button type="button" class="btn btn-primary" @click="modalClick()">
+      打開 Modal
+    </button>
+    <Modal ref="modalTemplate" />
+  </div>
+</template>
 
 <style lang="scss" scoped></style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,54 @@
+<script>
+import * as bootstrap from "bootstrap/dist/js/bootstrap.min.js";
+
+export default {
+  data() {
+    return {
+      // 先定義空物件 之後會把 Modal BS5 實體賦予在這邊
+      productModal: null,
+    };
+  },
+  methods: {
+    modalShow() {
+      this.productModal.show();
+    },
+    modalHide() {
+      this.productModal.hide();
+    },
+  },
+  mounted() {
+    // 在內層元件建立 Modal BS5 實體
+    this.productModal = new bootstrap.Modal(this.$refs.productModal, {
+      keyboard: true, // 按下ESC是否可以關閉
+    });
+  },
+};
+</script>
+
+<template>
+  <div class="modal fade" ref="productModal" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">
+            <slot name="modal-title">Modal Title</slot>
+          </h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <slot name="modal-body">Modal Body</slot>
+        </div>
+        <div class="modal-footer">
+          <slot name="modal-footer">Modal Footer</slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
